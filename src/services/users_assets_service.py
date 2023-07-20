9afd271d6fff4730fec45d7bbf128a527a17af07
Richard <richard_mbs@hotmail.com>
2023-07-20 13:05:30 -0300

    correcao do bug de atualizar quantidade de ativos

--- src/services/users_assets_service.py
+++ src/services/users_assets_service.py
@@ -1,5 +1,13 @@
+from enum import Enum
+
 from src.infra.repositories import UsersAssetsRepository
 from src.domain import UsersAssets
+
+
+class UpdateActionType(Enum):
+    DELETE = "DELETE"
+    BUY = "BUY"
+    UPDATE = "UPDATE"
 
 
 class UsersAssetsService:
@@ -8,7 +16,13 @@ class UsersAssetsService:
     def __init__(self):
         self.repository = UsersAssetsRepository()
 
-    def put_item(self, user_id: int, asset_code: str, quantity: int):
+    def put_item(
+        self,
+        user_id: int,
+        asset_code: str,
+        quantity: int,
+        action: UpdateActionType = UpdateActionType.BUY,
+    ):
         users_assets = self.repository.find_by_user_id_and_asset_code(
             user_id, asset_code
         )
@@ -17,6 +31,9 @@ class UsersAssetsService:
             return self.__create(
                 user_id=user_id, asset_code=asset_code, quantity=quantity
             )
+
+        if action == UpdateActionType.BUY:
+            quantity += users_assets.quantity
 
         self.repository.update_quantity(
             user_id=user_id, asset_code=asset_code, quantity=quantity
